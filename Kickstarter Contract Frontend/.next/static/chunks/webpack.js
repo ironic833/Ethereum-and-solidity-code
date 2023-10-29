4cb60575dc2a413ee7e4bdb494f471d5831750fc
Oisin Hickey <39494199+ironic833@users.noreply.github.com>
2023-10-29 18:05:02 +0000

    Updates
    - Added the basics of the UI, it's layout and the header and footer

--- Kickstarter Contract Frontend/.next/static/chunks/webpack.js
+++ Kickstarter Contract Frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e32845e3b962fa0a"; }
+/******/ 		__webpack_require__.h = function() { return "a97cb495d3d58bc7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
